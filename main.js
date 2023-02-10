f9ba59ab661eec65f8dffa0e312dc7313732909d
SofiaFesenko <108746952+SofiaFesenko@users.noreply.github.com>
2023-02-10 21:21:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+let a = Number(prompt("first number"))
+let b = Number(prompt("second number"))
+
+if (a + b > 10) {
+    alert("ok")
+}
+else {
+    alert("not ok")
+}
+
+
+let y_name = prompt("write your name")
+
+if (y_name == "Andrew") {
+    alert("hi")
+}
+else {
+    alert("who are you")
+}
+
+// if (((a + b)*2) > 20) {
+//     alert("big")
+// }
+// else {
+//     alert("small")
+// }
+
+// if (a > b) {
+// alert(a + " (first is bigger)")
+// }
+// else if (b > a) {
+//     alert(b + " (second is bigger)")
+// }
+// else {
+//     alert(a + b)
+// }
+
+// if (a + b > 10) {
+//     alert(10)
+// }
+// else {
+//     alert(a + b)
+// }
